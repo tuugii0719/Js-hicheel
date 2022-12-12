1bf7373f62306e2a7cc4bc85680277caf99d6b36
Evening <evening@MStarss-Anar.local>
2022-12-12 18:06:25 +0800

    12.12

---
+++ 12.12/stopwatch/main.js
@@ -0,0 +1,5 @@
+const p1 = document.createElement("p1").innerHtml="Ariin ungiig uurchilmuur baival tovchluur deer darnuu!"
+const p2 = document.createElement("p2").innerHtml="Ariin ungiig uurchilmuur baival tovchluur deer darnuu!"
+let Mongol=document.getElementById('Mongol')
+document.Mongol.appendChild(p1)
+div.appendChild(p2)
